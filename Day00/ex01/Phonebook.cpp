13725c3f3b72f4f130bfec625a07f81027aac4f5
hakamdev <abdouhakam8@gmail.com>
2021-07-23 17:55:42 +0100

    Added Phonebook.cpp NOT FINISHED

--- Day00/ex01/Phonebook.cpp
+++ Day00/ex01/Phonebook.cpp
@@ -6,9 +6,28 @@
 /*   By: hakamgo <hakamgo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 17:45:14 by hakamgo           #+#    #+#             */
-/*   Updated: 2021/07/23 17:51:10 by hakamgo          ###   ########.fr       */
+/*   Updated: 2021/07/23 17:55:13 by hakamgo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
 
+Phonebook::Phonebook( void ) {
+	this->currentIndex = 0;
+}
+
+Phonebook::~Phonebook( void ) {
+	
+}
+
+void	Phonebook::run( void ) {
+	
+}
+
+void	Phonebook::addContact( void ) {
+	
+}
+
+void	Phonebook::searchContact( void ) {
+	
+}
