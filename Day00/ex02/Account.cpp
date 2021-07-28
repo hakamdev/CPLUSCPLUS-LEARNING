e7f8aebb0a34026a91b3f924d7878581745bb033
hakamdev <abdouhakam8@gmail.com>
2021-07-28 17:15:41 +0100

    ex02: created Acount.cpp, (ex01 not finished)

---
+++ Day00/ex02/Account.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakamgo <hakamgo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/28 17:09:06 by hakamgo           #+#    #+#             */
+/*   Updated: 2021/07/28 17:14:55 by hakamgo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ) {
+	
+}
+
+void	Account::makeDeposit( int deposit ) {
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+
+}
+
+int		Account::checkAmount( void ) const {
+
+}
+
+void	Account::displayStatus( void ) const {
+
+}
+
+
+/* STATIC MEMBERS */
+int	Account::getNbAccounts( void ) {
+	
+}
+
+int	Account::getTotalAmount( void ) {
+	
+}
+
+int	Account::getNbDeposits( void ) {
+	
+}
+
+int	Account::getNbWithdrawals( void ) {
+	
+}
+
+void	Account::displayAccountsInfos( void ) {
+	
+}
