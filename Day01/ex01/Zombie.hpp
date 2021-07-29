b74246876e0a184391453434a148e2c645aac640
hakamdev <abdouhakam8@gmail.com>
2021-07-29 15:32:25 +0100

    Day01: ex01: started, (D00 ex01 not finished)

---
+++ Day01/ex01/Zombie.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakamgo <hakamgo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/29 15:20:46 by hakamgo           #+#    #+#             */
+/*   Updated: 2021/07/29 15:22:58 by hakamgo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ZOMBIE_HPP__
+# define __ZOMBIE_HPP__
+
+# include <iostream>
+
+using std::string;
+
+class Zombie {
+
+	private:
+		string	name;
+
+	public:
+		Zombie( void );
+		Zombie( string name );
+		~Zombie( void );
+		
+		string	getName( void );
+		void	setName( string name );
+		
+		void	announce( void );
+};
+
+#endif /* __ZOMBIE_HPP__ */
