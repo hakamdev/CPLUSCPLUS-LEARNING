2e1f5c9442e525d8a0193d571a6b23f73e72d28b
Elkebir Hakam <ehakam@estudent.1337.ma>
2021-09-02 16:35:30 +0100

    D03: ex00: created ClapTrap.hpp , (D02 && D00 ex01 & D01 ex04 not finished)

---
+++ Day03/ex00/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehakam <ehakam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 16:19:38 by ehakam            #+#    #+#             */
+/*   Updated: 2021/09/02 16:32:31 by ehakam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CLAPTRAP_HPP__
+# define __CLAPTRAP_HPP__
+
+# include <string>
+
+using std::string;
+
+class ClapTrap {
+	
+	private:
+		string	_name;
+		int		_hitPoints;
+		int		_energyPoints;
+		int		_attackDamage;
+	
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( ClapTrap& copy );
+		~ClapTrap( void );
+		ClapTrap&	operator = ( ClapTrap const & copy );
+		void		attack( std::string const & target );
+		void		takeDamage( unsigned int amount );
+		void		beRepaired( unsigned int amount );
+
+};
+
+#endif /* __CLAPTRAP_HPP__ */
